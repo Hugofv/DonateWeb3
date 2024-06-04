9e447a8e3e29ccba370d14a7e77ad1ee8c002474
Hugo Fernandes Vieira <hugo_fernandes2010@hotmail.com>
2024-06-03 21:28:24 -0300

    Create DonateHelp.sol

---
+++ smart-contract/DonateHelp.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+struct Request {
+    uint id;
+    address author;
+    string title;
+    string description;
+    string contact;
+    uint timestamp;
+    uint goal;
+    uint balance;
+    bool open;
+}
+
+contract DonateHelp {
+    uint public lastId = 0;
+    mapping(uint => Request) public requests;
+
+    function openRequest(string memory title, string memory description, string memory contact, uint goal) public {
+        lastId++;
+        requests[lastId] = Request({
+            id: lastId,
+            title: title,
+            description: description,
+            contact: contact,
+            goal: goal,
+            balance: 0,
+            timestamp: block.timestamp,
+            author: msg.sender,
+            open: true
+        });
+    }
+
+    function closeRequest(uint id) public {
+        address author = requests[id].author;
+        uint balance = requests[id].balance;
+        uint goal = requests[id].goal;
+
+        require(requests[id].open && (msg.sender == author || balance >= goal), unicode"Você não pode fechar este pedido");
+        
+        requests[id].open = false;
+
+        if(balance > 0) {
+            requests[id].balance = 0;
+            payable(author).transfer(balance);
+        }
+    }
+
+    function donate(uint id) public payable {
+        requests[id].balance += msg.value;
+        if(requests[id].balance >= requests[id].goal)
+            closeRequest(id);
+    }
+
+}
